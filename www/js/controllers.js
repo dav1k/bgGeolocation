bb3980edd04d2080b802bb4453d5e7f6535269cf
dav1k <dave.klausz@icloud.com>
2016-05-07 22:34:46 -0700

    Implemented background geolocation in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,7 +29,45 @@ function HomeCtrl($scope, $ionicPlatform, GeoServices) {
       GeoServices.checkLocation(callback);
     }, heartbeat);
 
+    // backgroundGeoLocation IMPLEMENTATION
+    // NOTE: backgroundGeoLocation will cause errors unless its simulated properly!!
 
+    // backgroundGeoLocation success call
+    var bgSuccess = function(location) {
+      console.log('backgroundGeoLocation callback : ', vm.geoData);
+
+      // DO ALL OTHER BACKGROUND LOCATION IMPLEMENTATION HERE!!
+      // esp: HTTP POST requests, or POST to server
+
+      /*
+      IMPORTANT !!!
+      You must execute the finish method to inform the native plugin that we're done doing background tasks.
+      You must do this regardless if said tasks actually finished.
+      OTHERWISE, iOS/Android will kill the app for using too much background compute time.
+      */
+
+      backgroundGeoLocation.finish();
+    };
+
+    // backgroundGeoLocation failure call
+    var bgFailure = function(error) {
+      console.log('backgroundGeoLocation error', error);
+    };
+
+    // backgroundGeoLocation options
+    var bgOptions = {
+      notificationText: 'DISABLED',
+      desiredAccuracy: 10,
+      stationaryRadius: 20,
+      distanceFilter: 30,
+      debug: true, // Enable sound upon backgroundGeoLocation cycle
+      stopOnTerminate: false // Enable to clear settings upon app termination.
+    };
+
+    backgroundGeoLocation.configure(bgSuccess, bgFailure, bgOptions);
+    backgroundGeoLocation.start();
+
+    // ./ backgroundGeoLocation IMPLEMENTATION
   });
 
   // On View enter
